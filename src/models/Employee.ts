12a9ac3737063899f9b253a67f9a3a57f3d47d99
fGiordi <giordifungula@gmail.com>
2023-10-13 14:12:31 +0200

    update to the model file

--- src/models/Employee.ts
+++ src/models/Employee.ts
@@ -1,4 +1,4 @@
-import { Schema, model, Document } from 'mongoose';
+import { Schema, model  } from 'mongoose';
 import { Employee } from '../interfaces/Schemas';
 
 const employeeSchema = new Schema<Employee>({
