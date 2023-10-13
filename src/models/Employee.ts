814f35cb6a1cf4b73870085ced0a71a2ea862848
fGiordi <giordifungula@gmail.com>
2023-10-13 14:23:25 +0200

    added validation on schema

--- src/models/Employee.ts
+++ src/models/Employee.ts
@@ -4,11 +4,11 @@ import { Employee } from '../interfaces/Schemas';
 const employeeSchema = new Schema<Employee>({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  salutation: { type: String, required: true },
+  salutation: { type: String, required: true, enum: ['Mr.', 'Dr.', 'Ms.', 'Mrs', 'Mx'] },
   employeeNumber: { type: Number, required: true },
-  gender: { type: String, required: true },
+  gender: { type: String, required: true, enum: ['Male', 'Female', 'Unspecified'] },
   grossSalary: { type: Number, required: true },
-  profileColor: { type: String, required: true },
+  profileColor: { type: String, required: true, enum: ['Green', 'Blue', 'Red', 'Default'] },
 });
 
 const EmployeeModel = model<Employee>('Employee', employeeSchema);
