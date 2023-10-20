007131d87caeadb1f5d33b8823d47a1fa692be74
fGiordi <giordifungula@gmail.com>
2023-10-20 13:00:40 +0200

    fixed duplicate employee id bug

--- src/models/Employee.ts
+++ src/models/Employee.ts
@@ -5,12 +5,14 @@ const employeeSchema = new Schema<Employee>({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   salutation: { type: String, required: true, enum: ['Mr.', 'Dr.', 'Ms.', 'Mrs.', 'Mx.'] },
-  employeeNumber: { type: Number, required: true },
+  employeeNumber: { type: Number, required: true, unique: true, index: true },
   gender: { type: String, required: true, enum: ['Male', 'Female', 'Unspecified'] },
   grossSalary: { type: Number, required: true },
   profileColor: { type: String, required: true, enum: ['Green', 'Blue', 'Red', 'Default'] },
 });
 
 const EmployeeModel = model<Employee>('Employee', employeeSchema);
+
+EmployeeModel.createIndexes();
 
 export default EmployeeModel;
